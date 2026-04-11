0a29eca12d558f6539812620b1088c661ff934f7
Rahiq Rahman <rahiqrahman2001@gmail.com>
2026-04-11 17:02:52 +0600

    backend config added

---
+++ DPP/server/src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const routes = require('./routes');
+app.use('/api', routes);
+
+app.listen(5000, () => console.log('Server running on port 5000'));
