e87cd2df6ec590c84a75b062ec4ec3a40edb1a6a
Rahiq Rahman <rahiqrahman2001@gmail.com>
2026-04-17 16:44:14 +0600

    changed database name

--- DPP/server/src/config/db.js
+++ DPP/server/src/config/db.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',
-  password: '123',
+  password: 'rahiq',
   host: 'rahiq',
   port: 5432,
-  database: 'digital product passport',
+  database: 'dpp',
 });
 
 module.exports = pool;
