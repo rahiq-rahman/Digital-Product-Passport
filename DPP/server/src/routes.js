22445e1d3144a47e80a646b9e0b29a90847100c0
Rahiq Rahman <rahiqrahman2001@gmail.com>
2026-04-18 20:40:55 +0600

    added showroom routes

--- DPP/server/src/routes.js
+++ DPP/server/src/routes.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
 const authRoutes = require('./modules/auth/auth.routes');
 const productRoutes = require('./modules/products/product.routes');
+const showroomRoutes = require('./modules/showroom/showroom.routes');
 
 // Use auth routes
 router.use('/auth', authRoutes);
 
 // Use product routes
 router.use('/products', productRoutes);
+
+// Use showroom routes
+router.use('/showroom', showroomRoutes);
 
 module.exports = router;
 
